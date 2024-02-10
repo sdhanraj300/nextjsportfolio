7c71aa6fef1c14389e10e612301d3a514eba7c11
Dhanraj Singh <sdhanraj300@gmail.com>
2024-02-11 03:26:49 +0530

    completed about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,132 @@
-import React from 'react'
+import AnimatedText from "@/components/AnimatedText";
+import Head from "next/head";
+import Image from "next/image";
+import Layout from "@/components/Layout";
+import { useRef, useEffect } from "react";
+import { useMotionValue, useInView, useSpring } from "framer-motion";
+import profilePic from "../../public/images/profile/developer-pic-2.jpg";
+const AnimatedNumber = ({ value }) => {
+  const ref = useRef(null);
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, {
+    duration: 3000,
+  });
+  const isInView = useInView(ref, { once: true });
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
+
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
+
+  return <span ref={ref}></span>;
+};
 
 const about = () => {
   return (
-    <div>about</div>
-  )
-}
+    <>
+      <Head>
+        <title>
+          Dhanraj | About Page
+          <meta name="description" content="About page of Dhanraj" />
+        </title>
+      </Head>
 
-export default about
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText text="Passion Fuels Purpose!" className="mb-16" />
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div
+              className="col-span-3 flex flex-col items-start
+            justify-start 
+            "
+            >
+              <h2
+                className="mb-4 text-lg font-bold 
+                text-dark/75 uppercase "
+              >
+                Biography
+              </h2>
+              <p className="font-medium ">
+                Hi, I'm Dhanraj, an aspiring web developer and UI/UX designer
+                with a passion for creating beautiful, functional, and
+                user-centered digital experiences. I I am always looking for new
+                and innovative ways to bring my visions to life.
+              </p>
+              <p className="my-4 font-medium ">
+                I believe that design is about more than just making things look
+                pretty – it's about solving problems and creating intuitive,
+                enjoyable experiences for users.
+              </p>
+              <p className="font-medium ">
+                Whether I'm working on a website, mobile app, or other digital
+                product, I bring my commitment to design excellence and
+                user-centered thinking to every project I work on. I look
+                forward to the opportunity to bring my skills and passion to
+                your next project.
+              </p>
+            </div>
+            <div
+              className="col-span-3 relative h-max rounded-2xl
+          border-2 border-solid border-dark
+          bg-light p-8 
+          "
+            >
+              <div
+                className="absolute top-0 -rigt-3 -z-10
+                w-[102%] h-[103%] rounded-[2rem] bg-dark
+                "
+              />
+              <Image
+                src={profilePic}
+                alt={"Dhanraj"}
+                className="w-full
+            h-auto rounded-2xl 
+            "
+              />
+            </div>
+            <div
+              className="col-span-2  flex flex-col items-end justify-between
+            
+            "
+            >
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumber value={50} /> +
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Projects
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumber value={40} /> +
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Projects Completed
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumber value={2} /> +
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Years Experience
+                </h2>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default about;
