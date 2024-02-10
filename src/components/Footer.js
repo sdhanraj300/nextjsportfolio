1b7ece0a2eb1454c1dedcb56fe21460f41a25463
Dhanraj Singh <sdhanraj300@gmail.com>
2024-02-11 01:33:56 +0530

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,39 @@
-import React from 'react'
-
+import React from "react";
+import Layout from "./Layout";
+import Link from "next/link";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer
+      className="w-full border-t-2 border-solid border-dark
+    font-medium text-lg 
+    "
+    >
+      <Layout className="py-8 flex items-center justify-between">
+        <span>{new Date().getFullYear()} &copy; ALl rigths Reserved.</span>
+        <div className="flex items-center">
+          Build with <span className="text-primary text-2xl px-1 ">&#9825;</span>
+          <Link
+            href="https://www.linkedin.com/sdhanraj300"
+            className="underline
+          underline-offset-2
+          "
+            target={"_blank"}
+          >
+            Dhanraj
+          </Link>
+        </div>
+        <Link
+          href="https://www.linkedin.com/sdhanraj300"
+          className="underline
+          underline-offset-2
+          "
+          target={"_blank"}
+        >
+          Contact Me
+        </Link>
+      </Layout>
+    </footer>
+  );
+};
 
-export default Footer
-//sfgsjfskjfsjkf
+export default Footer;
